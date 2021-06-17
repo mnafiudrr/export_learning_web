b18834badfac90e6a4701f934441687a93f6b4c3
Arya Gamas Mahardika <aryagamas@gmail.com>
2021-06-17 16:00:33 +0700

    feat: master get logo api endpoitns

--- app/Http/Controllers/MasterController.php
+++ app/Http/Controllers/MasterController.php
@@ -4,7 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Master;
+
 class MasterController extends Controller
 {
-    //
+    public function getAppLogo()
+    {
+        $BASE_LOGO_DB_NAME = 'APP_LOGO'; // Set The 'name' Column in database to fetch app logo
+        $logo = Master::where('name', $BASE_LOGO_DB_NAME)->first()->value;
+    
+        return response()->json($logo, 200);
+    }
 }
