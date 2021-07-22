9123922b135ac447ade98e75a6f2f0679fa3b7ed
Arya Gamas Mahardika <aryagamas@gmail.com>
2021-07-23 04:26:39 +0700

    feat: show data on materi pages (removed dummy data)

--- resources/views/materi.blade.php
+++ resources/views/materi.blade.php
@@ -1,119 +1,63 @@
-@extends('partials.master')
+@extends('partials.master') @section('content')
+<div class="container-fluid">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">List Materi</h1>
+        <a
+            href="{{ route('materi.create') }}"
+            class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"
+            ><i class="fas fa-plus fa-sm text-white-50"></i> Tambah Materi</a
+        >
+    </div>
 
-@section('content')
-    <div class="container-fluid">
-
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">List Materi</h1>
-            <a href="{{route('materi.create')}}" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                    class="fas fa-plus fa-sm text-white-50"></i> Tambah Materi</a>
-        </div>
-
-        <div class="card shadow mb-4">
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>Materi</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tfoot>
-                            <tr>
-                                <th>Materi</th>
-                                <th class="text text-center">Aksi</th>
-                            </tr>
-                        </tfoot>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <a href="{{route('materi.show',1)}}" style="text-decoration: none">
-                                        Materi 1
-                                    </a>
-                                </td>
-                                <td>
-                                    <button class="d-none d-sm-inline-block shadow-sm btn-danger btn-sm" type="button">
-                                        Hapus
-                                    </button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table
+                    class="table table-bordered"
+                    id="dataTable"
+                    width="100%"
+                    cellspacing="0"
+                >
+                    <thead>
+                        <tr>
+                            <th>Materi</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                    <tfoot>
+                        <tr>
+                            <th>Materi</th>
+                            <th class="text text-center">Aksi</th>
+                        </tr>
+                    </tfoot>
+                    <tbody>
+                        @foreach($materis as $materi)
+                        <tr>
+                            <td>
+                                <a
+                                    href="{{ route('materi.show', $materi->id) }}"
+                                    style="text-decoration: none"
+                                >
+                                   {{$materi->title}}
+                            </td>
+                            <td>
+                                <button
+                                    class="
+                                        d-none d-sm-inline-block
+                                        shadow-sm
+                                        btn-danger btn-sm
+                                    "
+                                    type="button"
+                                >
+                                    Hapus
+                                </button>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
+</div>
 @endsection
