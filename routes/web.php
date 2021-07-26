d73362109db20e45678593ae53a98c160f07ad95
Arya Gamas Mahardika <aryagamas@gmail.com>
2021-07-26 22:39:01 +0700

    chore: tidy up routes

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,9 @@ Route::prefix('materi')->group(function () {
     Route::get('/', [MateriController::class, 'indexMateri'])->name('materi.index');
     Route::post('/', [MateriController::class,'store']);
     Route::get('/create', [MateriController::class, 'create'])->name('materi.create');
+    Route::put('/{id}/edit', [MateriController::class, 'update'])->name('materi.update');
     Route::get('/{id}/edit', [MateriController::class, 'edit'])->name('materi.edit');
+
     Route::get('/{id}', [MateriController::class, 'show'])->name('materi.show');
 });
 
@@ -39,14 +41,20 @@ Route::prefix('materi')->group(function () {
 Route::prefix('submateri')->group(function () {
     Route::post('/', [SubMateriController::class, 'store']);
     Route::get('/create', [SubMateriController::class, 'create'])->name('submateri.create');
+    Route::put('/{id}/edit', [SubMateriController::class, 'update'])->name('submateri.update');
+
     Route::get('/{id}/edit', [SubMateriController::class, 'edit'])->name('submateri.edit');
     Route::get('/{id}', [SubMateriController::class, 'show'])->name('submateri.show');
 });
 
+Route::prefix('subsubmateri')->group(function () {
+    Route::post('/', [SubSubMateriController::class, 'store']);
 
-Route::get('subsubmateri/create', [SubSubMateriController::class, 'create'])->name('subsubmateri.create');
-Route::get('subsubmateri/{id}', [SubSubMateriController::class, 'show'])->name('subsubmateri.show');
-Route::get('subsubmateri/{id}/edit', [SubSubMateriController::class, 'edit'])->name('subsubmateri.edit');
+    Route::get('/create', [SubSubMateriController::class, 'create'])->name('subsubmateri.create');
+    Route::get('/{id}', [SubSubMateriController::class, 'show'])->name('subsubmateri.show');
+    Route::get('/{id}/edit', [SubSubMateriController::class, 'edit'])->name('subsubmateri.edit');
+});
+
 
 Route::get('kontensubsubmateri/create', [KontenSubSubMateriController::class, 'create'])->name('kontensubsubmateri.create');
 Route::get('kontensubsubmateri/{id}', [KontenSubSubMateriController::class, 'show'])->name('kontensubsubmateri.show');
