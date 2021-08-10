dd0dedcc3cda11b99f0e246454b5c12fd34e4f38
rbagus02 <rbagus02@gmail.com>
2021-08-10 16:31:48 +0700

    edit footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
         <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Eksplorasi 2021</span>
+            <span>Eksporasi 2021</span>
         </div>
     </div>
 </footer>
