5bb913085d5ed0e0aab96a0356d85902ef6ce12d
rbagus02 <rbagus02@gmail.com>
2021-08-15 02:33:21 +0700

    edit halaman quis nambahin tombol

--- resources/views/pages/quis/quis-show.blade.php
+++ resources/views/pages/quis/quis-show.blade.php
@@ -33,7 +33,7 @@
         <div class="container-fluid">
             <div class="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 class="h3 mb-0 text-gray-800">List Soal Jawaban</h1>
-                <a href="/question/create?quisId={{$quis->id}}" class=" d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
+                <a href="{{ route('quis.edit', $quis->id) }}" class=" d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                     <i class="fas fa-plus fa-sm text-white-50"></i> Tambah Soal Jawaban
                 </a>
             </div>
